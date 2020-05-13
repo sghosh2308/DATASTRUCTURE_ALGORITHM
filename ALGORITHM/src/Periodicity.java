56cab193fad79e4aebf95efb01fab4a46dc04822
soumyadeep <ghoshsoumyadeep33@gmail.com>
2020-05-13 14:56:17 +0530

    Periodicity oF string. EX-"HOHOHO" is 2

---
+++ ALGORITHM/src/Periodicity.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Periodicity
+{
+    private static void compressString(String s)
+    {
+        int[] z=ZAlgo.z_algorithm(s);
+        for(int i=0;i<z.length;i++)
+        {
+            if(i+z[i]==s.length())
+            {
+                System.out.println(s.substring(0,i).length());
+                break;
+            }
+        }
+    }
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        String s=br.readLine();
+        compressString(s);
+    }
+}
